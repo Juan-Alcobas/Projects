807e7c34dec102466778745c2b63748a22903034
Juan-Alcobas <78450211+Juan-Alcobas@users.noreply.github.com>
2022-10-07 01:01:37 -0700

    Update Array2DNode.java

--- hw05/hw05/Array2DNode.java
+++ hw05/hw05/Array2DNode.java
@@ -26,12 +26,4 @@ public class Array2DNode<E> {
 	public String toString() {
 		return this.item.toString();
 	}
-
-//	public E getItem() {
-//		return item;
-//	}
-//
-//	public void setItem(E item) {
-//		this.item = item;
-//	}
 }
